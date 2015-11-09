1860de4be15619c4a22c2c36ac90c8192e5d3e3a
Sajini <sajini_pm@outlook.com>
2015-11-09 11:12:55 -0800

    Removed tanks from screen

--- Code/CharacterControl/ClientCharacterControlGame.cpp
+++ Code/CharacterControl/ClientCharacterControlGame.cpp
@@ -88,9 +88,9 @@ int ClientCharacterControlGame::initGame()
 
 #if !PE_API_IS_D3D11
 		{
-			for (int i = 0; i < 6; ++i)
-				((ClientGameObjectManagerAddon*)(pGameCtx->getGameObjectManagerAddon()))->createTank(
-				i, m_pContext->m_gameThreadThreadOwnershipMask);
+			//for (int i = 0; i < 6; ++i)
+			//	((ClientGameObjectManagerAddon*)(pGameCtx->getGameObjectManagerAddon()))->createTank(
+			//	i, m_pContext->m_gameThreadThreadOwnershipMask);
 		}
 #endif
 	}
